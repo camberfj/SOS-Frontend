d9ad1ff29228b03c90e011c67c0811ec08899ddd
Fran <franjc_17@hotmail.com>
2023-10-31 11:19:01 -0300

    Se eliminaron y generaron nuevos archivos por errores en el proyecto

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
+import { PoPageBlockedUserModule, PoPageLoginModule } from '@po-ui/ng-templates';
+import { PoModule } from '@po-ui/ng-components';
+import { DialogHomeComponent } from './dialog-home/dialog-home.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    LoginComponent,
+    DialogHomeComponent,
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    PoPageLoginModule,
+    PoModule,
+    PoPageBlockedUserModule,
+  ]
+})
+export class HomeModule { }
