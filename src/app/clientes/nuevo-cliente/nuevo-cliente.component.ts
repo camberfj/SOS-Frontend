ac56e4c5d3083f077b8837a41d88ff3abcf63298
Fran <franjc_17@hotmail.com>
2023-12-31 10:52:41 -0300

    Se creo el component nuevo-cliente dentro del modulo clientes

---
+++ src/app/clientes/nuevo-cliente/nuevo-cliente.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-cliente',
+  templateUrl: './nuevo-cliente.component.html',
+  styleUrls: ['./nuevo-cliente.component.scss']
+})
+export class NuevoClienteComponent {
+  isOpen: string = '';
+
+  constructor() {}
+
+  open() {}
+}
